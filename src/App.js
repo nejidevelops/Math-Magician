c51de1e1d407563fe9a615fbc3b1806ce2af2e4f
Newton Ombese <newtonombese1@gmail.com>
2023-08-11 15:44:25 +0300

    Import Quote component to App js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Calculator />
-    </>
+      <Quote />
+    </div>
   );
 }
 
