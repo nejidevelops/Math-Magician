486f938fd050f690c3eb36845e6dbd98364b81c3
Newton Ombese <newtonombese1@gmail.com>
2023-08-16 10:03:35 +0300

    Add routes to help in navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { Routes, Route } from 'react-router';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="App">
-        <Calculator />
-        <Quote />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
     </>
   );
 }
